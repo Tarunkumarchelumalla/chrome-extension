4fef4de70ff121b4cb52fe7c86c6fc0882eac714
Tarunkumarchelumalla <tarunchelumalla@gmail.com>
2022-11-14 17:58:56 +0530

    deleted

--- popup.js
+++ popup.js
@@ -1,9 +1,10 @@
 let button = document.getElementById("2");
-button.addEventListener("click", clickFunction);
+button.addEventListener("click", clickFunction); // getting the emelmnt using dom 
 var address;
 function clickFunction() {
 
     var  a =document.getElementById("ok");
+    // down below it is an api call that chrome provides to uswe can use it
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         var tab = tabs[0];
         var title = tab.title;
@@ -12,6 +13,6 @@ function clickFunction() {
         address = title;
      
     
-        a.innerHTML=address;
+        a.innerHTML=address;// id we got from above 
     });
   }
